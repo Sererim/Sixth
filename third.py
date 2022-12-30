c097b68b78a1b9fdd4c399eaae47d1e3aa4f04cf
Sererim <danielleymanw@gmail.com>
2022-12-30 15:26:59 +0300

    Third exercies is done. Using map for multiplication of items of a list.

--- third.py
+++ third.py
@@ -5,11 +5,30 @@ class Pairs:
     
     
     @staticmethod 
-    def get_pairs(foo: list[int]) -> list[int]:
-        pass
-    
+    def get_pairs(num_1, num_2) -> int:
+        return num_1 * num_2
+        
     
 def main() -> int:
+    foo: list[int] = []
+    bar: list[int] = []
+    
+    size: int = 0
+    num: int = 0
+    char:str = ""
+    
+    while True:
+        size = int(input("Enter the size of a list.\n"))
+        for i in range(size):
+            num = int(input(f"Enter {i + 1} number of the list.\n"))
+            foo.append(num)
+        bar = foo[::-1]
+        s = list(dict.fromkeys(map(Pairs.get_pairs, foo, bar)))
+        print(s)
+        char = input("To terminate the program enter Y or y.\n")
+        
+        if char in ['Y', 'y']:
+            break
     return 0
 
 
